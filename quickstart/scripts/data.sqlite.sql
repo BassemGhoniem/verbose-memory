ba8ef4c4d29166422c9e2fd0fb0c0152fbd1712f
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2016-04-15 19:55:43 +0200

    I'm in love with zend

---
+++ quickstart/scripts/data.sqlite.sql
@@ -0,0 +1,10 @@
+INSERT INTO guestbook (id, email, comment, created)
+VALUES ('1', 'ralph.schindler@zend.com',
+        'Hello! Hope you enjoy this sample zf application!',
+        DATETIME('NOW'));
+
+
+INSERT INTO guestbook (id, email, comment, created)
+VALUES ('2', 'foo@bar.com',
+        'Baz baz baz, baz baz Baz baz baz - baz baz baz.',
+        DATETIME('NOW'));
